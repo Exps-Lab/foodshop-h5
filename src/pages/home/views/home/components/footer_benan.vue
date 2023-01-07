c957d34d66ec2ca751e101f310bec0946b951b80
haihan <haihanh2@163.com>
2023-01-07 23:14:50 +0800

    perf: 添加备案信息

--- src/pages/home/views/home/components/footer_benan.vue
+++ src/pages/home/views/home/components/footer_benan.vue
@@ -4,11 +4,11 @@
       <span class="name">FoodShop.Fun </span>
       <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2023000026号-1</a>
     </p>
-<!--  todo 公安备案未完成  -->
-<!--    <p class="list">-->
-<!--      <img class="police-badge" src="../imgs/police_badge.png">-->
-<!--      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=" target="_blank">京公网安备 xxxxxxxx号</a>-->
-<!--    </p>-->
+    <!--  公安备案 -->
+    <p class="list">
+      <img class="police-badge" src="../imgs/police_badge.png">
+      <a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=" target="_blank">京公网安备11010502051596号</a>
+    </p>
   </section>
 </template>
 
@@ -28,6 +28,8 @@
         font-size: 16px;
         font-weight: bold;
         margin-right: 10px;
+        color: #bbb;
+        text-shadow: 1px 1px #ddd;
       }
       .police-badge {
         width: 20px;
