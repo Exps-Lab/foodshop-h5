4c490483baf29f740bb0305f0f5346c47fa6c7ff
haihan <haihanh2@163.com>
2023-07-23 21:46:50 +0800

    chore: 优化sourceMap打包

--- build/pre_upload.js
+++ build/pre_upload.js
@@ -1,5 +1,6 @@
+// 删除打包生成的sourceMap文件和对应js的sourceMap标记
 
-const { readdirSync, unlinkSync, existsSync } = require('fs')
+const { readdirSync, readFileSync, writeFileSync, unlinkSync, existsSync } = require('fs')
 const path = require('path')
 
 // 服务根目录
@@ -10,10 +11,17 @@ const removeSourceMapFiles = async () => {
   if (await existsSync(distFile)) {
     const files = await readdirSync(distFile)
     files.map(async file => {
-      const pathName = path.join(distFile, file)
-      if (pathName.includes('.map')) {
-        await unlinkSync(pathName)
-        console.log(`${file}文件已删除`)
+      const filePath = path.join(distFile, file)
+      // [note] 删除sourceMap
+      if (filePath.includes('.js.map')) {
+        await unlinkSync(filePath)
+        console.log(`${file}__文件已删除`)
+      } else if (filePath.includes('.js')) {
+        // [note] 删除对应js的sourceMap标识
+        const fileContentStr = await readFileSync(filePath).toString()
+        const sourceMapFlagIndex = fileContentStr.indexOf('//# sourceMappingURL')
+        const handledFileContent = fileContentStr.slice(0, sourceMapFlagIndex)
+        await writeFileSync(filePath, handledFileContent, { flag: 'w+' })
       }
     })
   }
